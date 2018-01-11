546ae751aa617a89a229a5a0c5b620595ca9b3fe
mwieckowsk <32486835+mwieckowsk@users.noreply.github.com>
2018-01-11 23:35:05 +0100

    Add files via upload

---
+++ ProjApp/Startup.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ProjApp.Data;
+using ProjApp.Models;
+using Microsoft.AspNetCore.Localization;
+
+namespace ProjApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<ProjAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ProjAppContext")));
+
+            // use en-US culture to avoid problems with decimal values (i.e money) validation
+            // some countries use comma ',' while others use dot '.' as a decimal separator
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.DefaultRequestCulture = new RequestCulture("en-US");
+            });
+
+            // use lowercase routing
+            services.AddRouting(options => options.LowercaseUrls = true);
+
+            // add automapper - used to map models to viewModels
+            services.AddAutoMapper();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
